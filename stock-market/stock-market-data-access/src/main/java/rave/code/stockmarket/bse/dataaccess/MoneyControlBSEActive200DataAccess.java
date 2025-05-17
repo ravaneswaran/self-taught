febd33f13e37e5b3b0c461211dacbdf1aee98db0
ravaneswaran <ravaneswaran@gmail.com>
2025-05-17 21:42:45 +0530

    active 200 entity is introduced

---
+++ stock-market/stock-market-data-access/src/main/java/rave/code/stockmarket/bse/dataaccess/MoneyControlBSEActive200DataAccess.java
@@ -0,0 +1,11 @@
+package rave.code.stockmarket.bse.dataaccess;
+
+import rave.code.stockmarket.AbstractDataAccess;
+import rave.code.stockmarket.bse.entity.MoneyControlBSEActive200Entity;
+
+public class MoneyControlBSEActive200DataAccess extends AbstractDataAccess<MoneyControlBSEActive200Entity> {
+
+    public MoneyControlBSEActive200DataAccess(Class<MoneyControlBSEActive200Entity> type) {
+        super(type);
+    }
+}
