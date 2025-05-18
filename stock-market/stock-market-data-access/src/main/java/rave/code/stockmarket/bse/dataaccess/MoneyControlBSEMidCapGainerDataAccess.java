f506a94e00939326b2a4f7d20d101ac42d5d5a2e
ravaneswaran <ravaneswaran@gmail.com>
2025-05-18 14:54:54 +0530

    bse mid cap gainer entity included

---
+++ stock-market/stock-market-data-access/src/main/java/rave/code/stockmarket/bse/dataaccess/MoneyControlBSEMidCapGainerDataAccess.java
@@ -0,0 +1,12 @@
+package rave.code.stockmarket.bse.dataaccess;
+
+import rave.code.stockmarket.AbstractDataAccess;
+import rave.code.stockmarket.bse.entity.MoneyControlBSEMidCapGainerEntity;
+import rave.code.stockmarket.bse.entity.MoneyControlBSESmallCapGainerEntity;
+
+public class MoneyControlBSEMidCapGainerDataAccess extends AbstractDataAccess<MoneyControlBSEMidCapGainerEntity> {
+
+    public MoneyControlBSEMidCapGainerDataAccess(Class<MoneyControlBSEMidCapGainerEntity> type) {
+        super(type);
+    }
+}
