00ba50778f071be43665c71c74dad8d47861125d
ravaneswaran <ravaneswaran@gmail.com>
2025-05-22 17:07:59 +0530

    spring boot web application module is included

---
+++ stock-market/bse-india/bse-india-springboot-webapp/src/test/java/rave/code/BseIndiaSpringbootWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package rave.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BseIndiaSpringbootWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
