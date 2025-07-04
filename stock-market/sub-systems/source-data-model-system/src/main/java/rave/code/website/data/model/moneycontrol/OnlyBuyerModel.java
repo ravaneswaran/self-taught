be957de6347861e6b323bcd7629e85c5ef416b39
ravaneswaran <ravaneswaran@gmail.com>
2025-07-04 21:44:12 +0530

    only buyers implentation is on its way

---
+++ stock-market/sub-systems/source-data-model-system/src/main/java/rave/code/website/data/model/moneycontrol/OnlyBuyerModel.java
@@ -0,0 +1,14 @@
+package rave.code.website.data.model.moneycontrol;
+
+public class OnlyBuyerModel extends BSEGenericActiveModel {
+
+    private String bidQuantity;
+
+    public String getBidQuantity() {
+        return bidQuantity;
+    }
+
+    public void setBidQuantity(String bidQuantity) {
+        this.bidQuantity = bidQuantity;
+    }
+}
--- stock-market/sub-systems/source-data-parser-system/src/main/java/rave/code/data/parser/html/moneycontrol/BSESensexParser.java
+++ stock-market/sub-systems/source-data-parser-system/src/main/java/rave/code/data/parser/html/moneycontrol/BSESensexParser.java
@@ -3,7 +3,6 @@ package rave.code.data.parser.html.moneycontrol;
 public class BSESensexParser extends BSEActiveParser {
 
     public BSESensexParser() {
-        //https://www.moneycontrol.com/stocks/marketstats/bse-mostactive-stocks/sensex-4/
         super("https://www.moneycontrol.com/stocks/marketstats/bse-mostactive-stocks/sensex-4/");
     }
 }
