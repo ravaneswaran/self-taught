328e95fe365aaaa799f231893e6cd61ffbd1f7bd
ravaneswaran <ravaneswaran@gmail.com>
2025-07-05 12:32:05 +0530

    holiday mailer job is now updated with the mail content and mail send...testing underway

--- stock-market/sub-systems/quartz-scheduler-system/src/main/java/rave/code/quartz/job/mailer/HolidayMailerJob.java
+++ stock-market/sub-systems/quartz-scheduler-system/src/main/java/rave/code/quartz/job/mailer/HolidayMailerJob.java
@@ -2,15 +2,24 @@ package rave.code.quartz.job.mailer;
 
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import rave.code.mail.java.ElectronicMail;
 import rave.code.quartz.job.AbstractQuartzJob;
+import rave.code.quartz.job.moneycontrol.trading.BSEActive100Job;
 import rave.code.stockmarket.dataaccess.HolidayDataAccess;
 import rave.code.stockmarket.entity.HolidayEntity;
 
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class HolidayMailerJob extends AbstractQuartzJob {
+
+    private static final Logger LOGGER = Logger.getLogger(HolidayMailerJob.class.getName());
 
     public static void main(String[] args) throws JobExecutionException {
         HolidayMailerJob holidayMailerJob = new HolidayMailerJob();
@@ -26,9 +35,28 @@ public class HolidayMailerJob extends AbstractQuartzJob {
         HolidayDataAccess stockMarketHolidayDataAccess = new HolidayDataAccess();
         List<HolidayEntity> entities = stockMarketHolidayDataAccess.findAll();
 
+        String mailContent = "";
+        byte[] bytes = new byte[500];
+        InputStream inputStream = this.getClass().getResourceAsStream("/holiday_mail.html");
+        try {
+            int noOfBytesRead = inputStream.read(bytes);
+            if(-1 < noOfBytesRead){
+                mailContent = new String(bytes);
+            }
+        } catch (IOException ioException) {
+            LOGGER.log(Level.SEVERE, ioException.getMessage(), ioException);
+        }
+
+        ElectronicMail electronicMail = new ElectronicMail();
         for (HolidayEntity entity : entities) {
             if (formattedToDate.trim().equals(entity.getHolidate().trim())) {
-                System.out.println("SENDING MAIL....");
+                LOGGER.log(Level.INFO, "SENDING MAIL....");
+                mailContent = String.format(mailContent, entity.getHoliday(), entity.getDescription());
+                try {
+                    electronicMail.sendMail("noreply@stockmarket.con", "ravaneswaran@gmail.com", "Holiday Remainder", mailContent);
+                } catch (MessagingException messagingException) {
+                    LOGGER.log(Level.SEVERE, messagingException.getMessage(), messagingException);
+                }
             }
         }
     }
