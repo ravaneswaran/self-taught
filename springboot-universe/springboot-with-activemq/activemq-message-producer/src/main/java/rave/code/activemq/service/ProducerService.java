fbf51301e530a5b08675b0e30379b5e36b6bb954
ravaneswaran <ravaneswaran@gmail.com>
2025-07-18 21:02:26 +0530

    both producer and consumer are up and running in activemq module, testing pending

---
+++ springboot-universe/springboot-with-activemq/activemq-message-producer/src/main/java/rave/code/activemq/service/ProducerService.java
@@ -0,0 +1,57 @@
+package rave.code.activemq.service;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import rave.code.activemq.model.Department;
+import rave.code.activemq.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+import jakarta.jms.TextMessage;
+import java.util.Date;
+
+@Component
+public class ProducerService {
+
+    @Value("${spring.activemq.topic}")
+    String topic;
+
+    @Value("${spring.activemq.queue}")
+    String queue;
+
+    @Autowired
+    JmsTemplate jmsTemplate;
+
+    public void sendToQueue() throws JsonProcessingException {
+        try {
+            Employee employee = new Employee("Micheal", "Jackson", 10000L, new Date(), 24);
+            String jsonObj = new ObjectMapper().writer().withDefaultPrettyPrinter().writeValueAsString(employee);
+            jmsTemplate.send(queue, messageCreator -> {
+                TextMessage message = messageCreator.createTextMessage();
+                message.setText(jsonObj);
+                return message;
+            });
+        }
+        catch (Exception ex) {
+            System.out.println("ERROR in sending message to queue");
+        }
+    }
+
+    public void sendToTopic() throws JsonProcessingException {
+        try {
+            Department department = new Department(1,"HR",100);
+            String jsonObj = new ObjectMapper().writer().withDefaultPrettyPrinter().writeValueAsString(department);
+            jmsTemplate.send(topic, messageCreator -> {
+                TextMessage message = messageCreator.createTextMessage();
+                message.setText(jsonObj);
+                return message;
+            });
+        }
+        catch (Exception ex) {
+            System.out.println("ERROR in sending message to queue");
+        }
+    }
+}
