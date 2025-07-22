f170e6772ad79b90323b5a7d216adcb5fc293287
ravaneswaran <ravaneswaran@gmail.com>
2025-07-22 23:27:22 +0530

    springboot-jpa-with-hsqldb implementation is complete on local file...docker pending

---
+++ springboot-universe/springboot-jpa-with-hsqldb/src/main/java/rave/code/jpa/hsqldb/SpringBootJPAWithHSQLDBApplication.java
@@ -0,0 +1,11 @@
+package rave.code.jpa.hsqldb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJPAWithHSQLDBApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJPAWithHSQLDBApplication.class, args);
+    }
+}
