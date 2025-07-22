06f143558fcc66403da417a5b40112cd2881d76d
ravaneswaran <ravaneswaran@gmail.com>
2025-07-22 19:18:56 +0530

    springboot-jpa-with-apache-derby is checking with partial implementation

---
+++ springboot-universe/springboot-jpa-with-apache-derby/src/main/java/rave/code/jpa/derby/SpringBootJPAWithDerbyApplication.java
@@ -0,0 +1,11 @@
+package rave.code.jpa.derby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJPAWithDerbyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJPAWithDerbyApplication.class, args);
+    }
+}
---
+++ springboot-universe/springboot-jpa-with-apache-derby/src/main/java/rave/code/jpa/derby/request/PersonRequest.java
@@ -0,0 +1,64 @@
+package rave.code.jpa.derby.request;
+
+import java.util.Date;
+
+public class PersonRequest {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+    private int age;
+    private Date modifiedDate;
+
+    public PersonRequest(){
+        this.modifiedDate = new Date();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(Date modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+}
