0749b4fae81034becce82e591e6c8c255db724eb
ravaneswaran <ravaneswaran@gmail.com>
2025-07-23 16:31:15 +0530

    springboot-jpa-with-h2 implementation complete with liquibase standalone error

---
+++ springboot-universe/springboot-jpa-with-h2/src/main/java/rave/code/jpa/h2/SpringBootJPAWithH2Application.java
@@ -0,0 +1,11 @@
+package rave.code.jpa.h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJPAWithH2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJPAWithH2Application.class, args);
+    }
+}
