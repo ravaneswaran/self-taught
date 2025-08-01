97aaa508b1422a0ecce32def9ada9c938b1a206c
ravaneswaran <ravaneswaran@gmail.com>
2025-08-01 11:24:35 +0530

    spring-boot-jpa-with-db2 implementation done with testing

---
+++ springboot-apps/sql-database/springboot-jpa-with-db2/src/main/java/rave/code/jpa/db2/SpringBootJPAWithDB2Application.java
@@ -0,0 +1,11 @@
+package rave.code.jpa.db2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJPAWithDB2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJPAWithDB2Application.class, args);
+    }
+}
