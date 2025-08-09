f3f07e6b722f94cbeb3e07f7d95d58be719f89f4
ravaneswaran <ravaneswaran@gmail.com>
2025-08-10 01:36:39 +0530

    quartz maven plugins for corresponding database are in place and still to go...

---
+++ maven-plugins/quartz-hsqldb-maven-plugin/src/main/resources/tables-drop.sql
@@ -0,0 +1,11 @@
+DROP TABLE qrtz_locks IF EXISTS;
+DROP TABLE qrtz_scheduler_state IF EXISTS;
+DROP TABLE qrtz_fired_triggers IF EXISTS;
+DROP TABLE qrtz_paused_trigger_grps IF EXISTS;
+DROP TABLE qrtz_calendars IF EXISTS;
+DROP TABLE qrtz_blob_triggers IF EXISTS;
+DROP TABLE qrtz_cron_triggers IF EXISTS;
+DROP TABLE qrtz_simple_triggers IF EXISTS;
+DROP TABLE qrtz_simprop_triggers IF EXISTS;
+DROP TABLE qrtz_triggers IF EXISTS;
+DROP TABLE qrtz_job_details IF EXISTS;
